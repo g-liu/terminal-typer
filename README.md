008a189857d51147e884eb9c778ed30b507a6186
Geoffrey Liu <main@g-liu.com>
2014-04-20 20:38:56 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,11 @@
+HTML Terminal
+==========
+
+Simulates the look of a terminal, with HTML and CSS. No JavaScript!
+
+How it's done
+==========
+
+The basic idea is that it's simply a `div` spanning 100% width and height of the page. The trick to making it typable is the `contenteditable` attribute.
+
+Beyond that, it's all about the styling to get that retro terminal look!
---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<!--
+		Terminal Typer
+
+		Possibly the easiest way to simulate the look of a terminal. You can type on it too!
+
+		Made by Geoffrey Liu
+		g-liu.com
+		github.com/g-liu
+	-->
+
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, user-scalable=no" />
+	<title>Terminal typer</title>
+	
+	<link href="style.css" type="text/css" rel="stylesheet" />
+</head>
+<body>
+	<div id="terminal" contenteditable>Possibly the easiest way to simulate the look of a terminal.<br /><br />Type stuff here.</div>
+</body>
+</html>
